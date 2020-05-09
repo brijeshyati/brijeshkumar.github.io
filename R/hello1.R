e1b58954fbd6c627d1e2ea148523fbd27fd90149
brijeshyati <amar.biet@gmail.com>
2020-05-09 13:01:52 +0530

    Update hello1.R

--- R/hello1.R
+++ R/hello1.R
@@ -20,17 +20,6 @@ library("ggplot2")
 library("gridExtra")
 library("grid")
 
-url1 <- "https://api.covid19india.org/data.json"
-
-mydata <- fromJSON(url1)
-
-DF1 <- mydata[["cases_time_series"]]     #### india cases_time_series
-DF1$year <- 2020
-DF1$dateyear = paste(DF1$date,DF1$year)
-DF1$dateyear <- anydate(DF1$dateyear)
-
-DF1 <- DF1[rev(order(as.Date(DF1$dateyear, format = "%Y-%m-%d"))),]
-DF1 <- DF1[,c("dateyear","dailyconfirmed","totalconfirmed","dailydeceased","totaldeceased","dailyrecovered","totalrecovered")]
 
 
 }
